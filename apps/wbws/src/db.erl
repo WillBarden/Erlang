75f79b83ace40cd7c5f88e566beed02eccc0f788
WillBarden <wbarden@xby2.com>
2021-04-01 13:40:35 -0400

    Add port to db connection host string

--- apps/wbws/src/db.erl
+++ apps/wbws/src/db.erl
@@ -4,12 +4,13 @@
 
 connect() -> 
     Host = string:trim(os:getenv("DB_HOST", "localhost")),
+    Port = string:trim(os:getenv("DB_PORT", "5432")),
     Username = string:trim(os:getenv("DB_USERNAME", "username")),
     Password = string:trim(os:getenv("DB_PASSWORD", "password")),
     Database = string:trim(os:getenv("DB_DATABASE", "database")),
     Timeout = list_to_integer(string:trim(os:getenv("DB_TIMEOUT", "3000"))),
     case epgsql:connect(#{
-        host => Host, username => Username, password => Password, database => Database, timeout => Timeout
+        host => Host ++ ":" ++ Port, username => Username, password => Password, database => Database, timeout => Timeout
     }) of
         {ok, Conn} -> Conn;
         Other -> Other
