51bb842fe9516a001426f2115cd1302ea169ca9e
WillBarden <wbarden@xby2.com>
2021-04-01 15:48:35 -0400

    Remove unnecessary log statements and add headers to html

--- apps/wbws/src/register_h.erl
+++ apps/wbws/src/register_h.erl
@@ -3,10 +3,6 @@
 -export([init/2]).
 
 handle_get(Req) ->
-    io:fwrite("Getting registration page~n", []),
-    io:fwrite("Priv Dir: ~s~n", [code:priv_dir(wbws)]),
-    Results = epgsql:equery(db:connect(), "select id from users where username = $1", ["myusername"]),
-    io:write(Results),
     ResPath = filename:join(code:priv_dir(wbws), "register.html"),
     case file:read_file(ResPath) of
         { ok, Data } -> cowboy_req:reply(200, #{}, Data, Req);
@@ -14,7 +10,6 @@ handle_get(Req) ->
     end.
 
 handle_post(Req) ->
-    io:fwrite("Posting new user if~n", []),
     { ok, Body, _ } = cowboy_req:read_urlencoded_body(Req),
     { _, Username } = lists:keyfind(<<"username">>, 1, Body),
     { _, Password } = lists:keyfind(<<"password">>, 1, Body),
@@ -26,7 +21,6 @@ handle_post(Req) ->
     end.
 
 handle_unsupp_method(Req) ->
-    io:fwrite("Unsupported verb for registration~n", []),
     cowboy_req:reply(405, Req).
 
 init(Req, State) ->
